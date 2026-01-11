6a63fc653db43abd5f4b1298d411c42821b12eca
andisultan <andi.m.sultan@gmail.com>
2026-01-11 15:12:56 +0700

    chore: setup vitest and coverage

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['./src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{tsx,ts}'],
+
+    coverage: {
+      enabled: true,
+      include: ['src/**/*.{ts,tsx}']
+    },
+  }
 })
